19e883e0262c68551f1dfe08492e4d3bb29ce47c
Carlota Bindner <cev.bindner@gmail.com>
2018-03-21 18:10:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,7 @@
 class Dog
   attr_accessor :name, :breed, :id
 
-  def initialize(id = nil, name:, breed:)
-    @id = id
+  def initialize(name:, breed:)
     @name = name
     @album = album
   end
