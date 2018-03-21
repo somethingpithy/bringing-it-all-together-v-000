c74194fc623e52ece7607dbf2f29b231e0a187a5
Carlota Bindner <cev.bindner@gmail.com>
2018-03-21 18:14:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,5 +7,27 @@ class Dog
     @breed = breed
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXIST dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXIST dogs
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+
+
 
 end
