aa8b1845ee4af5917facb3662697bd68c9109e97
Carlota Bindner <cev.bindner@gmail.com>
2018-03-21 18:28:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,9 +28,55 @@ class Dog
   end
 
   def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
   end
 
+  def self.create
+    dog = Dog.new(name, breed)
+    dog.save
+  end
 
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+    SQL
 
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE songs
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+  def self.find_or_create_by
+  end
+
+  def new_from_db
+  end
+
+  def new_from_db
+  end
+
+  def find_by_name
+  end
 
 end
